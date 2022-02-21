e78e8647b05a38d94018966fc693ad40d32c6195
AndrMshk <andrmshk1@gmail.com>
2022-02-21 18:05:07 +0200

    21 02 22

---
+++ Maximum Triplet Sum.js
@@ -0,0 +1,4 @@
+function maxTriSum(numbers){
+    const x = [...new Set(numbers.sort((a, b) => b - a))]
+    return x[0]+x[1]+x[2]
+  }
---
+++ Odd-Even String Sort.js
@@ -0,0 +1,8 @@
+function sortMyString(S) {
+    x = ''
+    y = ''
+    for (let i in S) {
+      i % 2 ===0 ? x += S[i] : y += S[i]
+    }
+      return x + ' ' + y;
+  }
